9f45cb145c80169a05760d90f42792b916c2fb76
sserw <b0ld@web.de>
2025-08-03 17:35:05 +0200

    Initial commit

---
+++ src/main/java/com/example/ai_agent/AiAgentSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAgentSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAgentSandboxApplication.class, args);
+	}
+
+}
